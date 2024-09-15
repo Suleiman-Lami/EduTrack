0caf5618aa090daf2a65077ff4d022bba077aab0
Valpeterson247 <anunobivalentine24@gmail.com>
2024-09-15 13:46:38 +0100

    FooterWork

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -9,23 +9,41 @@ const Footer = () => {
     const currentYear = currentDate.getFullYear();
   
   return ( 
-    <div className='Footer'>
-      <div className="footerWrap">
-        <div className="footerTop">
-        <div className="footerHead">
-          
+    <footer className='Footer'>
+      {/* <div className="footerWrap"> */}
+        <div className="footerExtras">
+        <div className="footerExtra first-child">
+        <div className="logoWrapper">
             <img src={Logo}/>
         
           <h2>EDUTRACK</h2>
+
         </div>
-        <span>Keep track of your studies with EDUTRACK Easily manage your assignments, grades, and progress all in one place</span>
+        <p>EduTrack makes sure every school day matters. Our app makes attendance easy to track, keeps parents informed, and helps schools keep students on track for success.</p>
+        </div>
+        <div className='footerExtra second-child'> 
+          <ul>
+            <li>Company</li>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Features</li>
+          <li>Pricing</li>
+          </ul>
+          <ul>
+            <li>Support & Log In</li>
+            <li>Contact Us</li>
+            <li>Admin Login</li>
+            <li>Teachers Login</li>
+            <li>Parents/Students Login</li>
+          </ul>
+        </div>
         </div>
         <hr />
         <div className="footerBottom">
           <span>©{currentYear} Edutrack app . All Rights Reserved. Terms & Privacy</span>
         </div>
-      </div>
-    </div>
+      {/* </div> */}
+    </footer>
   
   )
 }
